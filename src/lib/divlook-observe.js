4edba588387ba1357107d4ce94773d8c02888fca
GrayBug <uihyeon.cha@gmail.com>
2018-05-15 01:55:36 +0900

    getter setter 추가

--- src/lib/divlook-observe.js
+++ src/lib/divlook-observe.js
@@ -1,9 +1,5 @@
 'use stric'
 
-const {
-  log
-} = require('./console')
-
 const defaultOtions = {
   watch: {}
 }
@@ -11,32 +7,31 @@ const defaultOtions = {
 const Observe = (data = {}, {
   watch = defaultOtions.watch
 } = defaultOtions) => {
+
   /**
-   * @type {{ data: {}, set: (data: {}) => void }}
+   * 관찰
+   * @param {{}} origin 원본 객체
+   * @param {{}} result 등록할 객체
+   * @param {string[]} keyname key 배열
    */
-  let observe = {
-    data,
-    get set() {}
-  }
-
-  // set property 선언
-  Object.defineProperty(observe, 'set', {
-    enumerable: false,
-    configurable: false,
-    get() {
-      return (cb) => {
-        let insert
-        if (typeof cb === 'function')
-          insert = cb(data)
-        else if (typeof cb === 'object' && cb !== null)
-          insert = cb
-        else
-          return
-
-        execWatchMethods(insert)
+   const regObserve = (origin, result = {}, keyname = []) => {
+    for (let key in origin) {
+      let val = origin[key]
+      if (isObject(val)) {
+        regObserve(val, result[key], keyname.concat(key))
+      } else {
+        Object.defineProperty(result, key, {
+          get() {
+            return val
+          },
+          set(value) {
+            execWatchMethods(object2level1(value, {}, keyname.concat(key)))
+            val = value
+          }
+        })
       }
-    },
-  })
+    }
+  }
 
   /**
    * 1단계 객체로 변환.
@@ -46,7 +41,7 @@ const Observe = (data = {}, {
    * @returns {{[key: string]: ['oldVal', 'newVal']}}
    */
   const object2level1 = (obj, result = {}, keyname = []) => {
-    if (typeof obj === 'object' && obj !== null && JSON.stringify(obj) !== '{}') {
+    if (isObject(obj)) {
       for (let key in obj) {
         let childVal = obj[key]
         object2level1(childVal, result, keyname.concat(key))
@@ -59,18 +54,47 @@ const Observe = (data = {}, {
 
   /**
    * watch에 등록된 객체가 입력되면 실행.
-   * @param {{}} inputData
+   * @param {{}} level1object
    */
-  const execWatchMethods = (inputData) => {
-    let resultObj = object2level1(inputData)
-    for (let key in resultObj) {
+  const execWatchMethods = (level1object) => {
+    for (let key in level1object) {
       if (key in watch) {
         let fn = watch[key]
-        fn.length === 1 ? fn(resultObj[key][1]) : fn(...resultObj[key])
+        fn.length === 1 ? fn(level1object[key][1]) : fn(...level1object[key])
       }
     }
-    return Object.assign(data, inputData)
   }
+
+  const isObject = (obj) => typeof obj === 'object' && obj !== null
+
+  /**
+   * @type {{ data: {}, set: (data: {}) => void }}
+   */
+  let observe = {
+    data: data,
+    get set() {}
+  }
+
+  regObserve(data, observe.data)
+
+  // set property 선언
+  Object.defineProperty(observe, 'set', {
+    enumerable: false,
+    configurable: false,
+    get() {
+      return (cb) => {
+        let insert
+        if (typeof cb === 'function')
+          insert = cb(data)
+        else if (isObject(cb))
+          insert = cb
+        else
+          return
+
+        execWatchMethods(object2level1(insert))
+      }
+    },
+  })
 
   return observe
 }
